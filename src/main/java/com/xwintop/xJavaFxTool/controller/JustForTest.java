5dd77704bb8dba3181d369e0051c9e7117ae8924
renzhaoyang <renchaoyang@oppo.com>
2021-10-15 16:23:13 +0800

    testing with sonar issues.

--- src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
@@ -1,8 +1,17 @@
 package com.xwintop.xJavaFxTool.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class JustForTest {
 
     public static void main(String[] args) {
+        List<String> as = new ArrayList<>();
+        as.add("hello world");
         System.out.println("hello world");
+        Collections.emptyList();//for sonarlint
+        System.out.println("hello world");
+
     }
 }
