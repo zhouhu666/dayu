ea834eae65090f83852a5cbb16687855cc2b03af
renzhaoyang <renchaoyang@oppo.com>
2021-10-25 15:59:02 +0800

    gray push

--- src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * test class
+ */
 public class JustForTest {
     public static final String TOGEN_ACB = "123123123";
 
     public static void main(String[] args) {
         List<String> as = new ArrayList<>();
         as.add("hello world ");
+        //add for gray push
         System.out.println("hello world");
         Collections.emptyList();//for sonarlint
         System.out.println("hello world");
