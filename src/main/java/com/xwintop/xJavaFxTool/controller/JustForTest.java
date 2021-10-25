29f3f947fb8ea982bb280782310e44da00c2c8da
renzhaoyang <renchaoyang@oppo.com>
2021-10-25 16:14:21 +0800

    gray push z1

--- src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
@@ -13,7 +13,7 @@ public class JustForTest {
     public static void main(String[] args) {
         List<String> as = new ArrayList<>();
         as.add("hello world ");
-        //add for gray push 。
+        //add for gray push 。2
         System.out.println("hello world");
         Collections.emptyList();//for sonarlint
         System.out.println("hello world");
