be5619ab8c8e35b2b29d772edf8d5ee668466c31
W9010275 <v-chensanfeng@oppo.com>
2022-01-19 18:10:51 +0800

    fix(test): @formatter:off test 9

--- src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/JustForTest.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class JustForTest {
-    public static final String TOGEN_ACB = "123123123";
+    public static final String TOGEN_ACB = "123d123123";
 
     public static void main(String[] args) {
         List<String> as = new ArrayList<>();
