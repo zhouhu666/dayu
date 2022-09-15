e83c2e0983c2c03b7c5b21a1d6506622842b4eaf
renzhaoyang <renchaoyang@oppo.com>
2022-09-15 23:10:44 +0800

    feat(test): %3013775 2222228
    1. 测试新逻辑推送

--- src/main/java/com/xwintop/xJavaFxTool/controller/Fa2.java
+++ src/main/java/com/xwintop/xJavaFxTool/controller/Fa2.java
@@ -2,10 +2,8 @@ package com.xwintop.xJavaFxTool.controller;
 
 /**
  * fa2 change1
- *
  */
 public class Fa2 {
-
     public static void main(String[] args) {
         System.out.println("Hello");
     }
